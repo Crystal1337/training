695dde9c2f720c745d12d1332b0d17e4e3900a4b
Crystal1337 <michal.nwk@gmail.com>
2020-08-07 00:17:26 +0200

    changed file name to echo.php and made first html/php basic file

---
+++ html.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+
+<html land="pl">
+
+<head>
+
+  <meta charset="utf-8">
+
+  <title>PHP/HTML reminder</title>
+
+  <style>
+    header
+    {
+      background: #e3e3e3;
+      padding: 2em;
+      text-align: center;
+    }
+    </style>
+
+</head>
+
+<body>
+
+  <header>
+
+    <h1><?php echo 'Hello World';?></h1>
+
+  </header>
+
+</body>
+
+</html>
