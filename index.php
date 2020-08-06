8d492b5b5b1832e9bbfcd9ed519a5af80b453ea9
Crystal1337 <michal.nwk@gmail.com>
2020-08-06 18:09:39 +0200

    Quick echo reminder

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+$greetings = 'Hello Planet';
+$name = 'Michał Nowak';
+$greeting = 'Hello';
+
+echo $greetings;
+echo '<br>';
+echo 'Hello World';
+echo '<br>';
+echo "Hello $name";
+echo '<br>';
+echo 'Hello ' . $name;
+echo '<br>';
+echo $greeting . ' ' . $name;
+echo '<br>';
+echo "{$greeting} {$name}";
+?>
